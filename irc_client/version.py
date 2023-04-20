a0141a60b857fcf8d2cfda62bbedf4b0bd95387d
AETH-erial <aetheriak@proton.me>
2023-04-20 12:38:18 -0500

    initial commit

---
+++ irc_client/version.py
@@ -0,0 +1,8 @@
+"""
+Holds the version information for the package
+"""
+__copyright__ = 'Copyright (c) 2023 aeth'
+__status__ = 'prod'
+
+__version_info__ = (1, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
